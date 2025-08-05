9f10a64db49691f36dd9ea969f0def91ab9f5e12
Rohit Dhakane <rohitdhakane6@gmail.com>
2025-08-05 23:02:40 +0530

    feat(eslint): add custom rule overrides for TypeScript and React

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Add your custom rule overrides
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
