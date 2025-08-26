517ad8d1b3c697e410aec5bb6fef4aaa9e9c2bbb
Rohit Dhakane <rohitdhakane6@gmail.com>
2025-08-26 22:11:42 +0530

    feat(database): add new user and workspace tables with relationships

--- drizzle/0001_deep_kid_colt.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "waitlist" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"email" varchar(150) NOT NULL,
-	"created_at" timestamp with time zone DEFAULT now(),
-	CONSTRAINT "waitlist_email_unique" UNIQUE("email")
-);
